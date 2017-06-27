1c59a078faa2394ab23054565546f2f81f28aed0
王灏 <wang.hao@dabofun.com>
2017-06-27 19:00:15 +0800

    no message

--- IGetNo.py
+++ IGetNo.py
@@ -128,22 +128,10 @@ def _IGetNo(sToken, sID, log):
 	#}
 	return sRetData
 #}
-#
-# def encrypt(sData, sKey, sIV=b'0000000000000000'):
-# #{
-# 	mode = AES.MODE_CBC
-# 	encryptor = AES.new(sKey, mode, sIV)
-# 	nAddLen = 16 - (len(sData) % 16)
-# 	sData = sData + ('\0' * nAddLen)
-# 	sRet = encryptor.encrypt(sData)
-# 	return b2a_hex(sRet)
-# #}
-#
-# def decrypt(sData, sKey, sIV = b'0000000000000000'):
-# #{
-# 	sData = a2b_hex(sData)
-# 	mode = AES.MODE_CBC
-# 	decryptor = AES.new(sKey, mode, sIV)
-# 	sRet = decryptor.decrypt(sData)
-# 	return sRet.rstrip('\0')
-# #}
+
+#-------------------------------------------------------#
+if __name__ == '__main__':
+#{
+	sToken = '11bad81b6772098ea2552f5a450eda063975202004e70634c32398b3971e37eeff2168373ec7aad411daa2cf9aa1a1c42daaaafab672f1024436728b018560fbd3e7b1b89f956c89c4bbbadd7aeed2ad3e5433c345ae61551494ac6ae73f9724'
+	_IGetNo(sToken, '28072', None)
+#}

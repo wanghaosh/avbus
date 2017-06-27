52c335b06986501b53479467c6d504f357a97d2f
王灏 <wang.hao@dabofun.com>
2017-06-27 17:53:18 +0800

    no message

--- IGetActorProgramList.py
+++ IGetActorProgramList.py
@@ -39,7 +39,7 @@ def _IGetActorProgramList(sActor, nPageIndex, nPageSize, log):
 
 	sMode = 'mem'
 	mem = CMemCached()
-	sKey = 'api_getactorprogramlist_' + sActor
+	sKey = 'api_getactorprogramlist_' + sActor + '_%d_%d'%(nPageIndex, nPageSize)
 	sKey = sKey.encode('utf-8')
 	sRet = mem.Get(sKey)
 

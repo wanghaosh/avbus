ca6bc0f32a000102d7f6a00a3fe58394bb520d5e
王灏 <wang.hao@dabofun.com>
2017-06-29 10:54:48 +0800

    no message

--- Tools/worms2.py
+++ Tools/worms2.py
@@ -13,7 +13,7 @@ import commands
 
 # from cassandra.cluster import Cluster
 # from cassandra.policies import DCAwareRoundRobinPolicy
-import mysql.connector
+# import mysql.connector
 
 import datetime
 #import memcache
@@ -27,7 +27,7 @@ import urllib
 
 import subprocess
 
-from libS3 import CS3Bucket
+# from libS3 import CS3Bucket
 # import boto
 import boto3
 # import botocore
@@ -83,12 +83,14 @@ class C1sdy:
 	def __init__(self):
 	#{
 		self.m_sBaseURI = 'http://www.1sdy.com/topiclist/'
+		self.m_dictActors = {}
 	#}
 
 	def GetAllPage(self):
 	#{
 		for nPageName in range(86, 513):
 		#{
+			nPageIndex = 0
 			while nPageIndex >= 0:
 			#{
 				if nPageIndex == 0:
@@ -100,12 +102,112 @@ class C1sdy:
 					nPageIndex = self.GetOnePage('%d-%d.html' % (nPageName, nPageIndex))
 				#}
 			#}
+			# break
 		#}
+		sData = json.dumps(self.m_dictActors, ensure_ascii=False)
+		f = open('/Users/wanghao/Develop/avbus/Tools/data_1sdy.txt', 'w')
+		f.write(sData)
+		f.close()
 	#}
 
 	def GetOnePage(self, sPageName):
 	#{
-		print sPageName
+		print '-------------------------------'
+		sUri = self.m_sBaseURI + sPageName
+		print sUri
+		sData = CHttp.HttpGet(sUri)
+		# print sData
+		sActor = sData.split('<title>')[1].split('所有作品番号')[0]
+		# print sActor
+		sPic = sData.split('.html"><img src="')[1].split('">')[0]
+		# print '    -> ' + sPic
+
+		sDetail = sData.split('简介</a>')[1].split('</div>')[0].replace('<br />', '\n').replace('<div class="u">', '').replace('</br>', '')
+		# print sDetail
+
+		self.AddActor(sActor, sPic, sDetail)
+
+		aryProgramsTmp = sData.split('<div class="showPlay">')[1].split('<a href="')#.split('.html" target="_blank"><img src="')
+		for sTmp in aryProgramsTmp:
+		#{
+			aryTmp = sTmp.split('" target="_blank"><img src="')
+			if len(aryTmp) <= 1:
+				continue
+			sDetailPageUri = 'http://www.1sdy.com' + aryTmp[0]
+
+			sOneProgram = aryTmp[1].split('"></a>')[0]
+			aryTmp = sOneProgram.split('" alt="')
+			sCoverPic = aryTmp[0]
+			sNo = aryTmp[1].split(' ')[1]
+			# print '    -> [' + sNo + '] ' + sDetailPageUri + ' / ' + sCoverPic
+			# print '    -> ' + sNo
+
+			# self.AddProgram(sActor, , sCoverPic, sNo, )
+			self.GetProgramDetailPage(sDetailPageUri, sActor, sNo, sCoverPic)
+		#}
+		sTmp = sData.split('页次:')[1].split('页')[0]
+		nCurPage = int(sTmp.split('/')[0])
+		nTotalPage = int(sTmp.split('/')[1])
+		print '[%d / %d]'%(nCurPage, nTotalPage)
+		if nCurPage >= nTotalPage:
+			return -1
+		return nCurPage + 1
+	#}
+
+	def GetProgramDetailPage(self, sUri, sActor, sNo, sCoverPic):
+	#{
+		sData = CHttp.HttpGet(sUri)
+		sData = sData.replace('\r', '').replace('\n', '').replace('<br>', '').replace('<p>', '')
+		aryTmp = sData.split('<li class="small">')
+		# for sTmp in aryTmp:
+		# #{
+		# 	print sTmp
+		# #}
+		sDT = aryTmp[3].replace('</li>', '')
+		sName = aryTmp[7].replace('</li>', '')#.replace('\n', '')
+		sInfo = sData.split('<div class="showInfo">')[1].split('</div>')[0]
+
+		self.AddProgram(sActor, sName.strip(' '), sNo, sCoverPic, sInfo.strip(' '), sDT.strip(' '))
+	#}
+
+	def AddActor(self, sActor, sActorPic, sDetail):
+	#{
+		print sActor + ' : ' + sActorPic
+		# print '  -> ' + sDetail
+		if self.m_dictActors.has_key(sActor) is False:
+		#{
+			self.m_dictActors[sActor] = {}
+		#}
+		self.m_dictActors[sActor]['pic'] = sActorPic
+		self.m_dictActors[sActor]['detail'] = sDetail
+		if self.m_dictActors[sActor].has_key('programs') is False:
+		#{
+			self.m_dictActors[sActor]['programs'] = []
+		#}
+	#}
+
+	def AddProgram(self, sActor, sProgramName, sProgramNo, sProgramCover, sProgramDetail, sDT):
+	#{
+		print '  -> [' + sProgramNo + '] ' + sDT + ' / ' + sProgramName
+		# print '  -> [' + sProgramNo + '] ' + sProgramName# + ' / ' + sProgramDetail
+		dictProgram = {
+				'name': sProgramName.strip(' '),
+				'no': sProgramNo.strip(' '),
+				'cover': sProgramCover.strip(' '),
+				'detail': sProgramDetail.strip(' '),
+				'dt': sDT.strip(' ')
+			}
+
+		self.m_dictActors[sActor]['programs'].append(dictProgram)
+
+		# download program cover
+		sCmd = 'curl -o "' + sActor + '_' + sProgramNo + '.jpg" --referer "http://www.1sdy.com" "' + sProgramCover + '" -A "Mozilla/5.0 (Windows NT 6.1)"'
+		print sCmd
+		os.system(sCmd)
+
+		f = open(sActor + '_' + sProgramNo + '.json', 'w')
+		f.write(json.dumps(dictProgram, ensure_ascii=False))
+		f.close()
 	#}
 #}
 

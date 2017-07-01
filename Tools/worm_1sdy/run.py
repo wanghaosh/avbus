7fbb0bde1e92af50938ebb4ec86917d15ab9fcfe
王灏 <wang.hao@dabofun.com>
2017-07-01 21:33:49 +0800

    no message

--- Tools/worm_1sdy/run.py
+++ Tools/worm_1sdy/run.py
@@ -56,7 +56,7 @@ class C1sdy:
 		# for nPageName in range(86, 513):
 		# for nPageName in range(86, 88):
 		# for nPageName in range(89, 101):
-		for nPageName in range(372, 513):
+		for nPageName in range(515, 516):
 		#{
 			a = COneActor(nPageName, g_mysql, s3, '/app/srv_api/Tools/worm_1sdy/data/')
 			a.GetAndParse()
